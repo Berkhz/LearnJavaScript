b27923c5f0c037c10831174f3682ce75e1290de7
Kauan Bertalha <kauan.hb2004@gmail.com>
2023-05-27 18:43:05 -0300

    Avaliacao

---
+++ avaliacao/avaliacao1/script.js
@@ -0,0 +1,3 @@
+// Atividade como parte de avaliação bimestral da disciplina de Algoritmos e Lógica de Programação II, a ser enviada até dia 04-06-2023.
+// Utilizando Javascript, construa um programa que leia um arquivo texto e alimente cada linha lida em um elemento de um vetor. Posteriormente, converta todos os caracteres em maiúsculo e classifique a tabela em ordem crescente. Apresente o vetor ao usuário no final.
+
